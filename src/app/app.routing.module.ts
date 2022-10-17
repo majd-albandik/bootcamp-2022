139d8a581b5ec81b79ee2bff53bfd8f7c1d33c82
Majd Albandik <majd.albandik@t-systems.com>
2022-10-17 21:14:52 +0200

    init commit
    add make logos
    
    added prettier
    
    add cars photos + prettier

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,17 @@
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: AppComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
