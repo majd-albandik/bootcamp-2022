26d4d10c36f8103c9897de068ef97ef3d6bdc48d
fanniszasz <fanni.szasz@t-systems.com>
2022-10-28 09:33:47 +0200

    change name

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent implements OnInit {
-  name = 'Majd'; //Change this with your name :)
+  name = 'Fanni'; //Change this with your name :)
 
   constructor() {}
 
