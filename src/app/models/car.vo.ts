6cd8664dd333a849bd7f7fe99d3ddc45b47f7fbf
fanniszasz <fanni.szasz@t-systems.com>
2022-10-28 09:43:28 +0200

    add more property for car

--- src/app/models/car.vo.ts
+++ src/app/models/car.vo.ts
@@ -1,8 +1,19 @@
 export interface Car {
-  id: number,
-  model: string,
-  firstRegistration: string,
+  id: number;
+  model: string;
+  firstRegistration: string;
+  origin: string;
+  description: string;
 }
 
+export interface carDetails {
+  kmPerGallon: number;
+  cylindres: number;
+  displacement: number;
+  horsePower: number;
+  weight: number;
+  acceleration: number;
+
+}
 
 
